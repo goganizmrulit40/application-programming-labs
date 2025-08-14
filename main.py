7816bca196b3053be12016759b7ec1645180b6b3
Maximord <maximyarosl@mail.ru>
2025-08-14 23:46:53 +0400

    Добавляем функцию чтения из файла, т.к. мы работаем с файлом. Соответствующий вызов в main.

--- main.py
+++ main.py
@@ -8,5 +8,12 @@ def parse_arguments():
     return args.filename
 
 
+def read_from_file(filename):
+    with open(filename, 'r', encoding='utf-8') as file:
+        text = file.read()
+    return text
+
+
 if __name__ == "__main__":
     filename = parse_arguments()
+    text = read_from_file(filename)
