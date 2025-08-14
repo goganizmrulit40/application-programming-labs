80215bf0f84a144525154a566257fd44078aad3f
Maximord <maximyarosl@mail.ru>
2025-08-14 23:43:32 +0400

    Добавляем функцию парсинга аргументов командной строки. Делаем по примеру из теории к лабе, только оборачиваем это в функцию.

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 import argparse
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('filename', type=str, help='Название текстового файла')
+    args = parser.parse_args()
+    return args.filename
+
+
+if __name__ == "__main__":
+    filename = parse_arguments()
