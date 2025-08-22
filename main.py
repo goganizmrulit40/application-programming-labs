cf1f0edbc1291eba2ca3828d738b8bae96923f68
Maximord <maximyarosl@mail.ru>
2025-08-22 19:24:38 +0400

    Пропишем импорт сторонней библиотеки в отдельной строке, также напишем новую функцию по считыванию/загрузке изображения из файла по примеру из теории. Сразу пропишем докстринг к этой функции. Функция возвращает изображение как массив numpy в формате BGR, т.е. в двоичном представлении цветных изображений.

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import argparse
 import sys
 
+import cv2
+
 
 def parse_arguments():
     """
@@ -14,9 +16,25 @@ def parse_arguments():
     return parser.parse_args()
 
 
+def load_image(image_path):
+    """
+    Загружает изображение из файла с помощью OpenCV
+    :param image_path: str, путь к файлу изображения
+    :return: массив NumPy с данными изображения в формате BGR
+    """
+    image = cv2.imread(image_path)
+    print("Успешная загрузка!")
+    if image is None:
+        raise ValueError(f"Не удалось загрузить изображение: {image_path}")
+    return image
+
+
 if __name__ == "__main__":
     try:
         args = parse_arguments()
+
+        print("Загрузка изображения...")
+        image = load_image(args.input_image)
 
     except Exception as e:
         print(f"Ошибка: {e}")
