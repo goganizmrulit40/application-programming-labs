83b892280372b77c1f88b5b9989674b66567a9b8
Maximord <maximyarosl@mail.ru>
2025-08-29 17:11:22 +0400

    Добавим из второй лабы csv файл для обработки. Напишем функцию парсинга аргументов, пока что у объекта парсера есть 2 аргумента - входной и выходной файлы и сразу пропишем докстринг этой функции

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
 import pandas as pd
+import argparse
+
+
+def parse_arguments():
+    """
+    Парсинг аргументов командной строки
+    :return: объект с распарсенными аргументами
+    """
+    parser = argparse.ArgumentParser(description='Обработка данных изображений')
+
+    parser.add_argument('-i', '--input', required=True,
+                        help='Входной CSV файл с путями к изображениям')
+
+    parser.add_argument('-o', '--output', default='processed_image_data.csv',
+                        help='Выходной CSV файл (по умолчанию: processed_image_data.csv)')
+
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    args = parse_arguments()
+
+    print(f"Входной файл: {args.input}")
