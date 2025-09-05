db16bdac4f915954cd136fa71606d6c7a51adb28
Maximord <maximyarosl@mail.ru>
2025-09-05 21:03:34 +0400

    Начнем писать инициализацию (конструктор) класса (сам он наследует от QMainWindow) super() возвращает временный объект, инициализируя QMainWindow, настраивает его и создаёт, затем установим размеры окна: в скобках указаны: x, y, ширина и высота. Инициализируем итератор и пути к файлу аннотации и папке с изображениями

--- main_window.py
+++ main_window.py
@@ -1,4 +1,15 @@
 import sys
-from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import (QApplication, QMainWindow)
 
 from main import ImageIterator
+
+
+class ImageViewerApp(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Просмотр датасета изображений")
+        self.setGeometry(100, 100, 800, 600)
+
+        self.iterator = None
+        self.annotation_file = None
+        self.folder_path = None
